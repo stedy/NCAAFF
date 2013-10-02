f62a235e52c38488d81ff0108d9298205f56869a
Zach Stednick <zstednic@fhcrc.org>
2013-10-02 10:43:12 -0700

    prep database

---
+++ schema_picks.py
@@ -0,0 +1,22 @@
+import csv
+import sqlite3
+
+connection = sqlite3.connect('site/ncaaff.db')
+connection.text_factory = str
+cursor = connection.cursor()
+connection.text_factory = lambda x: unicode(x, "utf-8", "ignore")
+
+cursor.execute('drop table if exists picks')
+cursor.execute('CREATE TABLE picks (Conf text, Team text, \
+				Picker text)')
+connection.commit()
+csvk = csv.reader(open("all_teamsclean.txt", 'rb'), delimiter = ",",
+					quotechar = '"')
+
+k = (csvk,)
+csvk.next()
+
+for k in csvk:
+    print k
+    cursor.execute('INSERT INTO picks VALUES (?,?,?)', k)
+connection.commit()
