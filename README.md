364a1a1a0b1a350e140e15b357206a4eb9054f5e
Zach Stednick <zstednic@fhcrc.org>
2013-11-21 11:47:13 -0800

    code cleanup

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ NCAAFF
 ======
 
 My NCAA fantasy football setup
+
+View example at zachstednick.com/ncaaff
--- site/ncaaff.py
+++ site/ncaaff.py
@@ -1,10 +1,5 @@
 import sqlite3
-import time
-from werkzeug import check_password_hash, generate_password_hash
-from datetime import datetime
-from flask import Flask, request, session, g, redirect, url_for \
-        , abort, render_template, flash
-
+from flask import Flask, g, render_template
 from contextlib import closing
 
 DATABASE = '/home/zachs/NCAAFF/site/ncaaff.db'
@@ -25,11 +20,11 @@ def init_db():
         db.commit()
 
 def query_db(query, args=(), one = False):
-	"""Queries the database and returns a list of dictionaries"""
-	cur = g.db.execute(query, args)
-	rv = [dict((cur.description[idx][0], value)
-		for idx, value in enumerate(row)) for row in cur.fetchall()]
-	return (rv[0] if rv else None) if one else rv
+    """Queries the database and returns a list of dictionaries"""
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+        for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
 
 @app.before_request
 def before_request():
@@ -42,46 +37,54 @@ def teardown_request(exception):
 
 @app.route('/', methods = ['GET', 'POST'])
 def start():
-    error = None
-    entries = query_db("""select picker, score from standings""", one = False)
-    return render_template('standings.html', error = error, entries = entries)
- 
-@app.route('/query', methods = ['GET', 'POST'])
-def query():
-	return render_template('query.html')
+    """Main start page showing snapshot of standings"""
+    entries = query_db("""select picker, score FROM standings""", one = False)
+    return render_template('standings.html', entries = entries)
 
 @app.route('/all_conferences', methods = ['GET', 'POST'])
 def all_conferences():
-    entries = query_db("""select conference, picker, school, conf, overall, confpercent, points from allconf""", one = False)
-    acc = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'ACC'""", one = False)
-    american = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'American'""", one = False)
-    cusa = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'Conference USA'""", one = False)
-    mwc = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'MWC'""", one = False)
-    mac = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'MAC'""", one = False)
-    big12 = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'Big 12'""", one = False)
-    big10 = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'Big Ten'""", one = False)
-    pac12 = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'Pac-12'""", one = False)
-    sec = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'SEC'""", one = False)
-    sunbelt = query_db("""SELECT conference, picker, school, conf, overall, 
-            confpercent, points from allconf WHERE conference = 'Sun Belt'""", one = False)
+    """Show results and points for all conferences"""
+    entries = query_db("""select conference, picker, school, conf, overall,
+            confpercent, points FROM allconf""", one = False)
+    acc = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE conference = 'ACC'""",
+            one = False)
+    american = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE conference = 'American'""",
+            one = False)
+    big10 = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE conference = 'Big Ten'""",
+            one = False)
+    big12 = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE conference = 'Big 12'""",
+            one = False)
+    cusa = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE
+            conference = 'Conference USA'""", one = False)
+    mwc = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE conference = 'MWC'""",
+            one = False)
+    mac = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE conference = 'MAC'""",
+            one = False)
+    pac12 = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE conference = 'Pac-12'""",
+            one = False)
+    sec = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE conference = 'SEC'""",
+            one = False)
+    sunbelt = query_db("""SELECT conference, picker, school, conf, overall,
+            confpercent, points FROM allconf WHERE conference = 'Sun Belt'""",
+            one = False)
     return render_template('all_conferences.html', entries = entries,
             acc=acc, american=american, cusa=cusa, mwc=mwc, big12=big12,
             mac=mac, big10=big10, pac12 = pac12, sec=sec, sunbelt=sunbelt)
 
-
-
 @app.route('/<name>')
 def name_results(name):
-    entries = query_db("""select Team, Conf, Picker from picks where Picker == ?""", [name], one = False)
+    """View results of an individual person"""
+    entries = query_db("""select Team, Conf, Picker FROM picks
+            WHERE Picker == ?""", [name], one = False)
     return render_template('name.html', entries = entries)
 
 if __name__ == '__main__':
